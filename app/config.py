e7f69c3f520dc55a01feff2d3505d1fc91c78f30
Utugoed <willyam.butcher@proton.me>
2023-01-23 17:26:34 +0300

    small edits

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "redis_db"
     REDIS_PORT: int = 6379
 
-    SECRET_AUTH_KEY: str
+    SECRET_AUTH_KEY: str = "8aba20e02ac80fa4add1fd01b5891021f79138ffefe15744d0188f5426ae3506"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 130
 
